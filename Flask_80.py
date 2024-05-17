84c1a2d3570644f39873fb82c6dd5b1b6f153a74
Sabaa <sba2.seman.3@gmail.com>
2024-05-17 13:28:10 +0300

    Polybot_aws_sabaa_project

--- Flask_80.py
+++ Flask_80.py
@@ -14,7 +14,7 @@ def health_check():
     return 'Ok', 200
 @app.route('/new')
 def newfnction():
-    return 'hi how are you ?'
+    return 'hi  are you ?'
 @app.route('/results/', methods=['GET'])
 def results():
     prediction_id = request.args.get('predictionId')
