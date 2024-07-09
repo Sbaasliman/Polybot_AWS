b0d5c8085913c94577194dd2b2600a24856f6e13
Sabaa <sba2.seman.3@gmail.com>
2024-07-09 13:59:59 +0300

    Polybot_aws_sabaa_project

--- Polybot/app_test.py
+++ Polybot/app_test.py
@@ -1,15 +1,15 @@
 import telebot
 import os
 TELEGRAM_TOKEN = ''
-TELEGRAM_CHAT_URL = 'https://atech-bot.click'  # Replace with your actual URL
-PATH_CERT = '/app/PUBLIC.pem'  # Replace with the correct path in your environment
+TELEGRAM_CHAT_URL = 'https://sabaa.atech-bot.click'  # Replace with your actual URL
+PATH_CERT = 'PUBLIC.pem'  # Replace with the correct path in your environment
 try:
     bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
     # Ensure path_cert exists and is accessible
     if os.path.exists(PATH_CERT):
         with open(PATH_CERT, 'rb') as cert_file:
-            result = bot.set_webhook(url=f'{TELEGRAM_CHAT_URL}/{TELEGRAM_TOKEN}/', timeout=60, certificate=cert_file)
+            result = bot.set_webhook(url=f'{TELEGRAM_CHAT_URL}/{TELEGRAM_TOKEN}/', certificate = open(PATH_CERT, 'r'),  timeout=60)
             print("Webhook set successfully:", result)
     else:
         print("Error: Certificate file not found at", PATH_CERT)
